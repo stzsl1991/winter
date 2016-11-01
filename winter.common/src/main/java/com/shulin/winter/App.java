03d3d50092f6ffabf1fbf550cfb95cfe5a0f3399
shulin.zhang <shulin.zhang@tqmall.com>
2016-11-01 17:34:04 +0800

    first commit

---
+++ winter.common/src/main/java/com/shulin/winter/App.java
@@ -0,0 +1,13 @@
+package com.shulin.winter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
