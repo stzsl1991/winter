971a8c205a793ea532a7757d88d29c3d34b57264
shulin.zhang <shulin.zhang@tqmall.com>
2016-11-01 22:11:20 +0800

    调整项目结构，添加dal层，数据访问层

--- winter.dal/src/test/java/com/shulin/winter/springtest/Test.java
+++ winter.dal/src/test/java/com/shulin/winter/springtest/Test.java
@@ -3,8 +3,6 @@ package com.shulin.winter.springtest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.util.HashMap;
-
 public class Test {
 
     @org.junit.Test
@@ -12,6 +10,5 @@ public class Test {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
         Person person = applicationContext.getBean("person",Person.class);
         person.show();
-        HashMap map = new HashMap();
     }
 }
